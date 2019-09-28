517421038d562956a860283b864e9828563fc2ef
Joaquim d'Souza <joaquimds@gmail.com>
2019-09-28 21:35:15 +0100

    remove credits

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,9 +6,7 @@ const Footer = () => {
   return (
     <div className='footer'>
       <p>
-        Developed by&nbsp;
-        <a href='https://campaignlab.org.uk' target='_blank' rel='noopener noreferrer'>CampaignLab</a> for&nbsp;
-        <a href='https://labour.org.uk/' rel='noopener noreferrer'>UK Labour</a>
+        Developed by [INSERT CREDITS HERE]
       </p>
     </div>
   )
