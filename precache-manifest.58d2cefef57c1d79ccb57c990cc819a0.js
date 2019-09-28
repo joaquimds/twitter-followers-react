809d969ce3c29322f88d98fd8c0f75e2178f3fe1
Joaquim d'Souza <joaquimds@gmail.com>
2019-09-28 19:15:36 +0100

    pages

---
+++ precache-manifest.58d2cefef57c1d79ccb57c990cc819a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a980ba6e5fa8abf3d08f43055f7bdff4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c44720461198261b633e",
+    "url": "/static/css/main.49f50dce.chunk.css"
+  },
+  {
+    "revision": "b41587c76466a95aba99",
+    "url": "/static/js/2.b67d1083.chunk.js"
+  },
+  {
+    "revision": "c44720461198261b633e",
+    "url": "/static/js/main.920c3a69.chunk.js"
+  },
+  {
+    "revision": "1a6160540722e3860d81",
+    "url": "/static/js/runtime-main.91cc778e.js"
+  }
+]);
